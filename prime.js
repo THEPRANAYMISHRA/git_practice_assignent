a0a85113dccad4229c9375eab93eea8399db4478
abhishek3042 <114894296+abhishek3042@users.noreply.github.com>
2022-12-08 18:24:10 +0530

    This is my solution :)

--- prime.js
+++ prime.js
@@ -2,14 +2,14 @@ let num=13;
 
 let count=0;
 
-for(let i=0;i<num;i++){
+for(let i=1;i<=num;i++){
     if(num%i==0){
-        count+
+        count++
     }
 
     if(count==2){
-        console.log(num,"Prime")
+        console.log(num," is Prime")
     }else{
-        console.log(num,"non-Prime")
+        console.log(num," is not-Prime")
     }
 }
